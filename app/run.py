ce16da3a2e09350b1eadb584133cfd09f62d4cbf
Kevin Wood <kevin.wood75@gmail.com>
2019-10-06 23:55:05 -0400

    added date and created module

--- app/run.py
+++ app/run.py
@@ -10,9 +10,9 @@ if len(sys.argv) < 2:
 ends = str(datetime.today().strftime('%Y-%m-%d'))
 
 count = 0
-print("{0}".format(str(sys.argv)))
 for symbol in sys.argv:
-    if count is not 0:
+    print("{}".format(str(symbol)))
+    if count is not 0:    
        mv_avg.stock_graph(ticker = symbol, starts = '2019-01-01', ends = ends)
        count += 1
 
