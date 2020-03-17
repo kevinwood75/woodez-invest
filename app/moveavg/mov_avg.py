256931ee06991b2e848871e3abaad348be84625b
Kevin Wood <kevin.wood75@gmail.com>
2020-03-16 22:34:07 -0400

    price chg

--- app/moveavg/mov_avg.py
+++ app/moveavg/mov_avg.py
@@ -12,7 +12,6 @@ def stock_graph(**kwargs):
     ends = kwargs['ends']
     yf.pdr_override()
     data = pdr.get_data_yahoo(ticker, start=starts, end=ends)
-    print(data)
     close = data['Adj Close']
     close.index = pd.to_datetime(close.index)
     sma200 = close.rolling(window=200).mean()
@@ -32,16 +31,21 @@ def stock_graph(**kwargs):
         'SMA 15': sma15,
         'SMA 20': sma20,
         'SMA 50': sma50,
-        'SMA 200': sma200
+        'SMA 200': sma200,
+        'Price Chg': sma200 - close
     })
+
+    chgprice = priceSma_df.tail(800)
+
 
     plt.style.use('fivethirtyeight')
     plt.figure(figsize = (12,6))
-    plt.plot(priceSma_df[starts:ends]['Adj Close'], label="{0} Adj Close".format(ticker), linewidth = 2)
-    plt.plot(priceSma_df[starts:ends]['SMA 15'], label='15 days rolling SMA', linewidth = 1.5)
-    plt.plot(priceSma_df[starts:ends]['SMA 20'], label='20 days rolling SMA', linewidth = 1.5)
-    plt.plot(priceSma_df[starts:ends]['SMA 50'], label='50 days rolling SMA', linewidth = 1.5)
-    plt.plot(priceSma_df[starts:ends]['SMA 200'], label='200 days rolling SMA', linewidth = 1.5)
+    plt.plot(chgprice[starts:ends]['Adj Close'], label="{0} Adj Close".format(ticker), linewidth = 2)
+    plt.plot(chgprice[starts:ends]['Price Chg'], label="{0} Price Chg".format(ticker), linewidth = 2) 
+    plt.plot(chgprice[starts:ends]['SMA 15'], label='15 days rolling SMA', linewidth = 1.5)
+    plt.plot(chgprice[starts:ends]['SMA 20'], label='20 days rolling SMA', linewidth = 1.5)
+    plt.plot(chgprice[starts:ends]['SMA 50'], label='50 days rolling SMA', linewidth = 1.5)
+    plt.plot(chgprice[starts:ends]['SMA 200'], label='200 days rolling SMA', linewidth = 1.5)
     plt.xlabel('Date')
     plt.ylabel('Adjusted closing price ($)')
     plt.title("{0} Price with Moving Average".format(ticker))
