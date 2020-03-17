cea75242904f92261f4c08afe92aabcea33d48c6
Kevin Wood <kevin.wood75@gmail.com>
2020-03-16 23:24:30 -0400

    chg price

--- app/run.py
+++ app/run.py
@@ -13,7 +13,8 @@ count = 0
 for symbol in sys.argv:
     if count > 0: 
        try:
-            mv_avg.stock_graph(ticker = symbol, starts = '2015-01-01', ends = ends)
+         ##   mv_avg.stock_graph(ticker = symbol, starts = '2015-01-01', ends = ends)
+            mv_avg.stock_graph(ticker = symbol, starts = '2015-01-01', ends = '2020-03-16')
        except:
             print("Date does not exist in data. exiting...")
              
